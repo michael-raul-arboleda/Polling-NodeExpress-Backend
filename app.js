a6979b6ce1279aac2b3b4ea93aa36dc667e7d894
michael-raul-arboleda <michael.raul.arboleda@gmail.com>
2019-05-31 17:28:23 -0400

    moved api/posts to seprate route file then imported it in app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 var bodyParser = require("body-parser");
 var mongoose = require("mongoose");
 
-var Post = require('./models/post');
+const postsRoutes = require("./routes/posts");
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -59,77 +59,22 @@ app.options("/*", function(req, res, next){
   res.send(200);
 });
 
-app.post("/api/posts", (req, res, next) => {
-  const post = new Post({
-      title: req.body.title,
-      content: req.body.content
-  });
-  post.save().then( result => {
-    res.status(201).json({
-      message: 'Post added successfully',
-      postId: result._id
-    });
-  });
-});
-
-app.get("/api/posts", (req, res, next) => {
-  Post.find()
-      .then(documents => {
-        console.log(documents);
-        res.status(200).json({
-          message: 'Post fetched succesfully',
-          posts: documents
-        });
-      });
-});
-
-app.get('/api/posts/:id', (req, res, next) => {
-   Post.findById(req.params.id).then(post => {
-    if (post){
-        res.status(200).json(post);
-    } else {
-        res.status(404).json({message: 'Post not found!'});
-    }
-   });
-});
-
-app.put('/api/posts/:id', (req, res, next) => {
-  const post = new Post({
-    _id: req.body._id,
-    title: req.body.title,
-    content: req.body.content
-  });
-  Post.updateOne({_id: req.params.id}, post)
-      .then(result => {
-        res.status(200).json({ message: 'Successful update!'})
-      });
-});
-
-app.delete("/api/posts/:id", (req, res, next) => {
-  console.log('Hello\t' + req.params.id);
-  Post.deleteOne({_id: req.params.id}).then(result => {
-    console.log(result);
-    res.status(200).json({message: 'Post Deleted!'});
-  });
-});
+app.use('/api/posts', postsRoutes);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+    next(createError(404));
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
-
-
-
 
 module.exports = app;
